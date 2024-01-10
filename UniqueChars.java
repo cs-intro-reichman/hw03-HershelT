45910b38e6b951c4828faf234111263ada6fdbdc
= <hershelthomas@gmail.com>
2024-01-10 12:23:39 +0200

    fixed lak of space

--- UniqueChars.java
+++ UniqueChars.java
@@ -22,7 +22,7 @@ public class UniqueChars {
         for (int i = 0; i< s.length();i++ ) {
             testLetter = s.charAt(i);
             for (int j = i + 1;j < s.length();j++) {
-                if (s.indexOf(testLetter, j) > 0) {
+                if (s.indexOf(testLetter, j) > 0 && testLetter != ' ') {
                     numberList.add(s.indexOf(testLetter, j));
                 }
             }
