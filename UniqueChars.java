c7ce23bea265ced2fb75d0f68d2cd2f3b1d30f0f
= <hershelthomas@gmail.com>
2024-01-10 10:10:13 +0200

    stuff

--- UniqueChars.java
+++ UniqueChars.java
@@ -4,8 +4,7 @@ import java.util.Set;
 /** String processing exercise 2. */
 public class UniqueChars {
     public static void main(String[] args) {
-        //String str = args[0];
-        String str = "hheellregeroohhhhhhweeeeeeeezyw";
+        String str = args[0];
         System.out.println(uniqueChars(str));
     }
 
@@ -21,16 +20,12 @@ public class UniqueChars {
         Set<Integer> numberList = new HashSet<Integer>();
         int count = 0;
         for (int i = 0; i< s.length();i++ ) {
-            System.out.println(i +":" + s.charAt(i) + " : " + testLetter);
             testLetter = s.charAt(i);
             for (int j = i + 1;j < s.length();j++) {
                 if (s.indexOf(testLetter, j) > 0) {
-                    System.out.println(s.indexOf(testLetter, j));
                     numberList.add(s.indexOf(testLetter, j));
                 }
             }
-        }
-        for (int i =0; i < s.length();i++) {
             if (!numberList.contains(i)) {
                 newString+=s.charAt(i);
             }
